8ea86fb4ee73b64a70b7a520468440013e00f18e
GeraldRF <74776482+GeraldRF@users.noreply.github.com>
2021-04-14 02:22:22 -0600

    Update verificarWidth.js

--- recursos/A_JS/verificarWidth.js
+++ recursos/A_JS/verificarWidth.js
@@ -4,11 +4,11 @@ function verificar(){
        
        if(screen.width<500) {
        
-       menu="https://grfixurdivice.000webhostapp.com/recursos/A_HTML/menuDispositivosW<500.html";
+       menu="https://geraldrf.github.io/Ecommerce_EMRA-Store/recursos/A_HTML/menuMobiles.html";
        
        }else{
        
-       menu="https://grfixurdivice.000webhostapp.com/recursos/A_HTML/menuDispositivosW>500.html"
+       menu="https://geraldrf.github.io/Ecommerce_EMRA-Store/recursos/A_HTML/menuPC.html"
       
        } 
        
